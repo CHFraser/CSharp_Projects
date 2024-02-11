2f5a8687df898c063107921a2c63b5c6dfd053c8
CHFraser <ciarahfraser@outlook.com>
2024-02-11 14:05:01 -0800

    Calculator App
    A simple calculator app that adds or multiplies two digits.

---
+++ CalculatorApp.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace FormCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBxValue1 = new System.Windows.Forms.TextBox();
+            this.txtBxValue2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblFirstValue = new System.Windows.Forms.Label();
+            this.lblSecondValue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBxValue1
+            // 
+            this.txtBxValue1.Location = new System.Drawing.Point(439, 77);
+            this.txtBxValue1.Name = "txtBxValue1";
+            this.txtBxValue1.Size = new System.Drawing.Size(105, 20);
+            this.txtBxValue1.TabIndex = 0;
+            this.txtBxValue1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtBxValue2
+            // 
+            this.txtBxValue2.Location = new System.Drawing.Point(439, 166);
+            this.txtBxValue2.Name = "txtBxValue2";
+            this.txtBxValue2.Size = new System.Drawing.Size(105, 20);
+            this.txtBxValue2.TabIndex = 1;
+            this.txtBxValue2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(478, 225);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(150, 75);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(181, 225);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(150, 75);
+            this.btnMultiply.TabIndex = 3;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.Yellow;
+            this.lblResult.Location = new System.Drawing.Point(147, 408);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(57, 20);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "label1";
+            this.lblResult.Visible = false;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(331, 332);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(147, 41);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblFirstValue
+            // 
+            this.lblFirstValue.AutoSize = true;
+            this.lblFirstValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstValue.Location = new System.Drawing.Point(214, 77);
+            this.lblFirstValue.Name = "lblFirstValue";
+            this.lblFirstValue.Size = new System.Drawing.Size(89, 20);
+            this.lblFirstValue.TabIndex = 6;
+            this.lblFirstValue.Text = "First Value:";
+            // 
+            // lblSecondValue
+            // 
+            this.lblSecondValue.AutoSize = true;
+            this.lblSecondValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecondValue.Location = new System.Drawing.Point(214, 166);
+            this.lblSecondValue.Name = "lblSecondValue";
+            this.lblSecondValue.Size = new System.Drawing.Size(113, 20);
+            this.lblSecondValue.TabIndex = 7;
+            this.lblSecondValue.Text = "Second Value:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Simple Calculator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSecondValue);
+            this.Controls.Add(this.lblFirstValue);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtBxValue2);
+            this.Controls.Add(this.txtBxValue1);
+            this.Name = "Form1";
+            this.Text = "Calculator App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBxValue1;
+        private System.Windows.Forms.TextBox txtBxValue2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblFirstValue;
+        private System.Windows.Forms.Label lblSecondValue;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CalculatorApp.cs
@@ -0,0 +1,90 @@
+﻿// ciara Fraser
+// December 1, 2023
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double val1;
+            double val2;
+            double result;
+
+            while (double.TryParse(txtBxValue1.Text, out val1) == false
+                                                            || val1 < 0) 
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "Value must be numeric and more than 0.";
+                txtBxValue1.Text = "0";
+                txtBxValue1.Focus();
+            }
+
+            while (double.TryParse(txtBxValue2.Text, out val2) == false
+                                                          || val2 < 0)
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "Value must be numeric and more than 0.";
+                txtBxValue2.Text = "0";
+                txtBxValue2.Focus();
+            }
+
+            result = val1 + val2;
+            lblResult.Text = "Result: " + result;
+            lblResult.Visible = true;
+           
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            double val1;
+            double val2;
+            double result;
+
+            if (double.TryParse(txtBxValue1.Text, out val1) == false
+                                                            || val1 < 0)
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "Value must be numeric and more than 0.";
+                txtBxValue1.Text = "0";
+                txtBxValue1.Focus();
+            }
+
+            else if (double.TryParse(txtBxValue2.Text, out val2) == false
+                                                          || val2 < 0)
+            {
+                lblResult.ForeColor = Color.Red;
+                lblResult.Text = "Value must be numeric and more than 0.";
+                txtBxValue2.Text = "0";
+                txtBxValue2.Focus();
+            }
+            else
+            {
+                result = val1 * val2;
+                lblResult.Text = "Result: " + result;
+            }
+            
+        }
+
+        // button reset
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtBxValue1.Text = "0";
+            txtBxValue2.Text = "0";
+        }
+    }
+}
