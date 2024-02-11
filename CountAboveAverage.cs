5b2262c42e7dca251eb2ef187a17644f676082c6
CHFraser <ciarahfraser@outlook.com>
2024-02-11 14:13:08 -0800

    CountAboveAverage Program
    A program that implements to random class to assign numbers to an array, then  calculates the average and counts how many numbers in the array are above average.

---
+++ CountAboveAverage.cs
@@ -0,0 +1,50 @@
+﻿//ciara Fraser
+// December 18, 2023
+//Creating an array and displaying the average.
+using System;
+using static System.Console;
+
+namespace CountAboveAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float total = 0;
+            int[] randomArray = new int[20];
+            Random rnd = new Random();
+            float higherThanAverage = 0;
+
+            for (int i = 0; i < randomArray.GetLength(0); i++)
+            {
+                randomArray[i] = rnd.Next(1, 11); //between 1 and 10
+            }
+
+            //Displaying the array of nubmers
+            Write("The numbers are: ");
+            for (int i = 0; i < randomArray.Length; i++)
+            {
+                Write("{0} ", randomArray[i]);
+                total += randomArray[i];
+
+                // check if the array element is higher than the average
+                
+             
+            }
+
+            float avgOfArray = total / 20;
+
+
+            for (int i = 0; i<randomArray.Length; i++) 
+            {
+                if (randomArray[i] > avgOfArray)
+                {
+                    higherThanAverage++;
+                }
+            }
+            WriteLine();
+            WriteLine("The average is {0:f2}", avgOfArray);
+            WriteLine("There are {0} numbers higher than the average", higherThanAverage);
+        }
+    }
+}
